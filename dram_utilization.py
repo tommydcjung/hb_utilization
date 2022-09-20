5774e355b699a4e7db7793018a62cc9a2eaf21d1
Tommy Jung <tommy.dc.jung@gmail.com>
2022-09-20 12:26:20 -0700

    dram util rule

--- dram_utilization.py
+++ dram_utilization.py
@@ -1,69 +1,50 @@
-class DramStat:
-  def __init__(self, timestamp, tag, channel_id, idle, refresh, read, write):
-    self.timestamp = timestamp
-    self.tag = tag
-    self.channel_id = channel_id
-    self.idle = idle
-    self.refresh = refresh
-    self.read = read
-    self.write = write
+import pandas as pd
 
-  def get_tag_type(self):
-    return (self.tag & 0xc0000000) >> 30
 
 def parse_dram_stat():
-  bg_stats = []
-  with open("blood_graph_stat.log") as f:
-    header = f.readline()
-    lines = f.readlines()
-    for line in lines:
-      words = line.strip().split(",")
-      timestamp =   int(words[0])
-      tag =         int(words[1])
-      channel_id =  int(words[2])
-      idle =        int(words[3])
-      refresh =     int(words[4])
-      read =        int(words[5])
-      write =       int(words[6])
-      bg_stat = DramStat(timestamp, tag, channel_id, idle, refresh, read, write) 
-      bg_stats.append(bg_stat)
+  df = pd.read_csv("blood_graph_stat.log")
+  tags = df["tag"]
+  timestamps = df["timestamp"]
 
-  
-  start_bg_stat = None
+  # find start and end timestamp
   start_timestamp = (2**32)-1
-  end_bg_stat = None
   end_timestamp = 0
+  for i in range(len(tags)):
+    tag = tags[i]
+    timestamp = timestamps[i]
+    tag_type = (0xc0000000 & tag) >> 30
 
-  for bg_stat in bg_stats:
-    tag_type = bg_stat.get_tag_type()
-
-    # kernel start tag
+    # kernel start
     if tag_type == 2:
-      if start_timestamp > bg_stat.timestamp:
-        start_timestamp = bg_stat.timestamp
-        start_bg_stat = bg_stat
-    # kernel end tag
+      if start_timestamp > timestamp:
+        start_timestamp = timestamp
+    # kernel end
     elif tag_type == 3:
-      if end_timestamp < bg_stat.timestamp:
-        end_timestamp = bg_stat.timestamp
-        end_bg_stat = bg_stat
+      if end_timestamp < timestamp:
+        end_timestamp = timestamp
 
+  # grab start/end df
+  start_df = df[df["timestamp"]==start_timestamp].iloc[0]
+  end_df   = df[df["timestamp"]==end_timestamp].iloc[0]
 
   # calculate utilization
   total_cycle = float(end_timestamp - start_timestamp)
-  idle_cycle = float(end_bg_stat.idle - start_bg_stat.idle)
-  refresh_cycle = float(end_bg_stat.refresh - start_bg_stat.refresh)
-  read_cycle = float(end_bg_stat.read - start_bg_stat.read)
-  write_cycle = float(end_bg_stat.write - start_bg_stat.write)
-  stall_cycle = float(total_cycle - idle_cycle - read_cycle - write_cycle - refresh_cycle)
+  refresh_cycle = float(end_df["refresh"] - start_df["refresh"])
+  busy_cycle = float(end_df["busy"] - start_df["busy"])
+  read_cycle = float(end_df["read"] - start_df["read"])
+  write_cycle = float(end_df["write"] - start_df["write"])
+  idle_cycle = total_cycle - refresh_cycle - busy_cycle - read_cycle - write_cycle
+
+
   print("--------------------------------")
   print("DRAM Utilization")
   print("--------------------------------")
-  print("Idle        = {:.2f} %".format(idle_cycle/total_cycle*100))
   print("Refresh     = {:.2f} %".format(refresh_cycle/total_cycle*100))
-  print("Stall       = {:.2f} %".format(stall_cycle/total_cycle*100))
-  print("Read        = {:.2f} %".format(read_cycle/total_cycle*100))
-  print("Write       = {:.2f} %".format(write_cycle/total_cycle*100))
+  print("Read        = {:.2f} %".format(2*read_cycle/total_cycle*100))
+  print("Write       = {:.2f} %".format(2*write_cycle/total_cycle*100))
+  print("Busy        = {:.2f} %".format((busy_cycle-read_cycle-write_cycle)/total_cycle*100))
+  print("Idle        = {:.2f} %".format(idle_cycle/total_cycle*100))
   print("--------------------------------")
-  print("Utilization = {:.2f} %".format((read_cycle+write_cycle)/total_cycle*100))
+  print("Utilization = {:.2f} %".format(2*(read_cycle+write_cycle)/(total_cycle-refresh_cycle)*100))
+  print("Busy cycles = {:.2f} %".format(((read_cycle+write_cycle)+busy_cycle)/(total_cycle-refresh_cycle)*100))
   print("--------------------------------")
