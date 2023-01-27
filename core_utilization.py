a14a4c5bbe47de2742f94e5e30ead1189be0ca79
Tommy Jung <tommy.dc.jung@gmail.com>
2023-01-26 21:12:04 -0800

    print stall and bubble fore core stat

--- core_utilization.py
+++ core_utilization.py
@@ -3,6 +3,34 @@ import pandas as pd
 
 FILENAME = "vanilla_stats.csv"
 
+STALL_LIST = [
+  "stall_depend_dram_load",
+  "stall_depend_group_load",
+  "stall_depend_global_load",
+  "stall_depend_idiv",
+  "stall_depend_fdiv",
+  "stall_depend_local_load",
+  "stall_depend_imul",
+  "stall_amo_aq",
+  "stall_amo_rl",
+  "stall_bypass",
+  "stall_lr_aq",
+  "stall_fence",
+  "stall_remote_req",
+  "stall_remote_credit",
+  "stall_fdiv_busy",
+  "stall_idiv_busy",
+  "stall_barrier",
+  "stall_remote_ld_wb",
+  "stall_ifetch_wait",
+  "stall_remote_flw_wb"
+]
+BUBBLE_LIST = [
+  "bubble_branch_miss",
+  "bubble_jalr_miss",
+  "bubble_icache_miss"
+]
+
 def parse_vanilla_stat():
   try:
     df = pd.read_csv(FILENAME)
@@ -19,8 +47,16 @@ def parse_vanilla_stat():
   #print(stall_cols)
   total_cycle = 0
   instr_cycle = 0
-  bubble_cycle = 0
-  stall_cycle = 0
+  total_bubble_cycle = 0
+  total_stall_cycle = 0
+
+  # for each bubble and stall type
+  each_bubble_cycle = {}
+  each_stall_cycle = {}
+  for col in bubble_cols:
+    each_bubble_cycle[col] = 0
+  for col in stall_cols:
+    each_stall_cycle[col] = 0
 
   # calculate cycle
   start_timestamp = (2**32)-1
@@ -39,9 +75,11 @@ def parse_vanilla_stat():
       total_cycle -= timestamp
       instr_cycle -= instr_total
       for col in bubble_cols:
-        bubble_cycle -= df[col][i]
+        total_bubble_cycle     -= df[col][i]
+        each_bubble_cycle[col] -= df[col][i]
       for col in stall_cols:
-        stall_cycle  -= df[col][i]
+        total_stall_cycle      -= df[col][i]
+        each_stall_cycle[col]  -= df[col][i]
       
     # kernel end
     elif tag_type == 3:
@@ -50,17 +88,27 @@ def parse_vanilla_stat():
       total_cycle += timestamp
       instr_cycle += instr_total
       for col in bubble_cols:
-        bubble_cycle += df[col][i]
+        total_bubble_cycle += df[col][i]
+        each_bubble_cycle[col] += df[col][i]
       for col in stall_cols:
-        stall_cycle  += df[col][i]
+        total_stall_cycle  += df[col][i]
+        each_stall_cycle[col]  += df[col][i]
   
   
   print("")
   print("--------------------------------")
   print("Core Utilization")
+
   print("--------------------------------")
-  print("Stall         = {:.2f} %".format(stall_cycle/total_cycle*100))
-  print("Bubble        = {:.2f} %".format(bubble_cycle/total_cycle*100))
+  for col in stall_cols:
+    print("{:<{w1}} = {:<{w2}} ({:.2f} %)".format(col, each_stall_cycle[col], each_stall_cycle[col]/total_cycle*100, w1=30, w2=12))
+  print("{:<{w1}} = {:<{w2}} ({:.2f} %)".format("Total Stall", total_stall_cycle, total_stall_cycle/total_cycle*100, w1=30, w2=12))
+
+  print("--------------------------------")
+  for col in bubble_cols:
+    print("{:<{w1}} = {:<{w2}} ({:.2f} %)".format(col, each_bubble_cycle[col], each_bubble_cycle[col]/total_cycle*100, w1=30, w2=12))
+  print("{:<{w1}} = {:<{w2}} ({:.2f} %)".format("Total Bubble", total_bubble_cycle, total_bubble_cycle/total_cycle*100, w1=30, w2=12))
+
   print("--------------------------------")
   print("Utilization   = {:.2f} %".format(instr_cycle/total_cycle*100))
   print("--------------------------------")
