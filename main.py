9764428151e1df807a219511451fd222c1d037d7
tj <tommy.dc.jung@gmail.com>
2023-08-30 10:43:19 -0700

    check if vc_stat is not None

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ dram_stat = dr.parse_dram_stat()
 dr.print_dram_stat(dram_stat)
 
 vc_stat = vc.parse_vcache_stat()
-vc.print_vcache_stat(vc_stat)
+if vc_stat is not None:
+  vc.print_vcache_stat(vc_stat)
 
 core_stat = core.parse_vanilla_stat()
 core.print_vanilla_stat(core_stat)
