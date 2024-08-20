3ad12601a3304aa5d140c79752da0b752affaef4
tj <tommy.dc.jung@gmail.com>
2024-08-20 12:38:39 -0700

    README and some comments on main.py

---
+++ README.md
@@ -0,0 +1,18 @@
+```
+# Introduction
+
+  hb_utilization repo is a collection of python scripts to parse and summarize/visualize HB manycore profiler outputs.
+
+
+# Requirements
+
+  There are some commonly used python packages required for this: pandas, matplotlib, numpy.
+
+
+# Commonly used scripts
+
+  Each script should have a comment header that describes how it is used and required inputs.
+
+  - main.py : this is the mainly used script, which summarizes DRAMsim3, vcache, core utilization.
+
+```
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+#
+#   main.py
+#
+#   This script summarizes DRAMsim3, vcache, and vanilla core stats.
+#   
+#   Input files:
+#   - blood_graph_stat.log
+#   - vcache_stats.csv
+#   - vanilla_stats.csv
+# 
+#   How to use:
+#   python main.py {path-to-input-files}   
+#
+
 import dram_utilization as dr
 import vcache_utilization as vc
 import core_utilization as core
@@ -9,16 +23,16 @@ import sys
 
 os.chdir(sys.argv[1])
 
-#dram_stat = dr.parse_dram_stat()
-#dr.print_dram_stat(dram_stat)
+dram_stat = dr.parse_dram_stat()
+dr.print_dram_stat(dram_stat)
 
-#vc_stat = vc.parse_vcache_stat()
-#if vc_stat is not None:
-#  vc.print_vcache_stat(vc_stat)
+vc_stat = vc.parse_vcache_stat()
+if vc_stat is not None:
+  vc.print_vcache_stat(vc_stat)
 
 core_stat = core.parse_vanilla_stat()
 core.print_vanilla_stat(core_stat)
-core.summarize_core_stall(core_stat)
+#core.summarize_core_stall(core_stat)
 
 #wh.parse_wh_link_stat()
 #rtr.parse_router_stat()
